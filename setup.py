05821122ba5ae669d2a94be65c95616bded329ed
Andrey Kan <andrey.kan@dowjones.com>
2025-09-27 00:48:32 -0400

    retries in windaloft and au

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="weather-au",
-    version="0.0.8",
+    version="0.0.9",
     author="Tony Allan",
     author_email="tony@apms.com.au",
     description="Australian Weather Data (from bom.gov.au)",
